33bf109a4f2554e1a56745e4864d5efa86314c09
Ha Rim Ku <hrku19@gmail.com>
2020-08-24 18:34:15 -0700

    task router

--- routes/task.js
+++ routes/task.js
@@ -41,4 +41,40 @@ taskRouter
             .catch(err => next(err));
     });
 
+taskRouter
+    .route('/:taskId')
+    .get((req, res, next) => {
+        Task.findById(req.params.taskId)
+            .then(task => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(task);
+            })
+            .catch(err => next(err));
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /tasks/${req.params.taskId}`);
+    })
+    .put((req, res, next) => {
+        Task.findByIdAndUpdate(req.params.taskId, {
+            $set: req.body
+        }, { new: true })
+            .then(task => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(task);
+            })
+            .catch(err => next(err));
+    })
+    .delete((req, res, next) => {
+        Task.findByIdAndDelete(req.params.taskId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
+    });
+
 module.exports = taskRouter; 
